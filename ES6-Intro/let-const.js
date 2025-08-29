a244c58618a076abe008b95799365c34fde2ec32
Badsha012 <badshasheikh361@gmail.com>
2025-08-29 21:23:49 +0600

    nine commit

---
+++ ES6-Intro/let-const.js
@@ -0,0 +1,16 @@
+// es6
+const name='Bangladesh'
+const countryName='BD' +  name;
+console.log(countryName);
+
+let price =120;
+price=150;
+console.log(price);
+
+const drees=['shirt','pant','jacket'];
+drees.push('maflar');
+console.log(drees)
+
+const student={name:'jaglul',marks:50}
+student.marks=60
+console.log(student)
