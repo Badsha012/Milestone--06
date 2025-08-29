0abc5e9b8454775846f3fb09fc7b3a331a9f89b5
Badsha012 <badshasheikh361@gmail.com>
2025-08-29 22:37:33 +0600

    six commit

---
+++ ES6-Intro/template_string.js
@@ -0,0 +1,16 @@
+const poem =`amm pata jora jora
+marbo chabuk chorbe ghora 
+ora babu ghur dara`
+console.log(poem)
+
+function  sum(num1,num2) {
+    const output=`sum of ${num1}  and ${num2}` 
+    console.log(output);
+    
+}
+sum(20,40)
+
+
+const price=100;
+const dis=`Discounted Price  is ${price*.20}`
+console.log(dis);
