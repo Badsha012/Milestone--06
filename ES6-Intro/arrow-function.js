b3359830725aa851d048186a81da085edbc683c8
Badsha012 <badshasheikh361@gmail.com>
2025-08-30 21:08:49 +0600

    five commit

---
+++ ES6-Intro/arrow-function.js
@@ -0,0 +1,14 @@
+function add(num1,num2) {
+    return num1+num2;
+    
+}
+
+// function expression
+
+const additon =function (num1,num2) {
+    return num1+num2;
+
+    
+}
+const result=additon(17,13);
+console.log(result);
