8b6d7b8e2387286e7749123bc1966f434f304aa0
Badsha012 <badshasheikh361@gmail.com>
2025-08-31 17:28:40 +0600

    ten commit

---
+++ ES6-Intro/filter.js
@@ -0,0 +1,6 @@
+const numbers=[1,5,16,10,45,2,7];
+const greaterhan10= numbers.filter(x => x>10)
+console.log(greaterhan10)
+
+const evenNumber= numbers.filter(num => num % 2 ==0);
+console.log(evenNumber)
