f093d39cf74c3ffaf2ebb32318c111a8eb8da032
Badsha012 <badshasheikh361@gmail.com>
2025-08-31 17:07:20 +0600

    nine commit

---
+++ ES6-Intro/map.js
@@ -0,0 +1,14 @@
+const number=[4,5,2,3,5];
+const doubled=[]
+for(const num of number){
+    const result =num*2;
+    doubled.push(result);
+
+}
+console.log(doubled);
+ const mul=[2,3,4,5,6]
+const doubleId = y => y*2
+
+
+const k=mul.map(doubleId)
+console.log(k);
---
+++ ES6-Intro/string_map.js
@@ -0,0 +1,5 @@
+const friends=['Zaved','Kamal','Nobal','Kodbel'];
+const namelengths=friends.map(name => name.length)
+console.log(namelengths)
+const firstLetter=friends.map(name => name[0]);
+console.log(firstLetter);
