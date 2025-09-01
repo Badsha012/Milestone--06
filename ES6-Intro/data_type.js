045ab178aeaceceb0dffdc6c4db84b0a5139e513
Badsha012 <badshasheikh361@gmail.com>
2025-09-01 09:14:46 +0600

    two commit

---
+++ ES6-Intro/data_type.js
@@ -0,0 +1,10 @@
+// primitve data type
+const data = 7;
+const number =9.00
+const isNum=true
+// non - primitive data type
+const student=[23,4,5,6];
+const suject=[
+    {name:'bangla',marks:78,},
+]
+
