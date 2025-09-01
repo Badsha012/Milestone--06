f6a8b5a07a121bf04b382b93cfbe30548f411e5f
Badsha012 <badshasheikh361@gmail.com>
2025-09-01 12:16:46 +0600

    six commit

---
+++ ES6-Intro/truthy.js
@@ -0,0 +1,15 @@
+let data;
+console.log('value of data',data);
+
+data='';
+data=' '
+data='0';
+data=false;
+data=true
+if(data){
+    console.log('Value of data');
+
+}
+else{
+    console.log('value of data not ');
+}
