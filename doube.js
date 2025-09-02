451662abf55dbbe7f0e67dd42c961af0979ebb8b
Badsha012 <badshasheikh361@gmail.com>
2025-09-02 20:26:23 +0600

    nine commit'

--- doube.js
+++ doube.js
@@ -1,3 +1,6 @@
 console.log( 2=== 2);
 console.log(1===2);
 console.log(1=='2');
+console.log(null===null);
+const p=[4,5,7,8];
+console([5]==[5]);
---
+++ scope.js
@@ -0,0 +1,6 @@
+if(true){
+    const data=58;
+
+}
+console.log(data);
+data=78;
